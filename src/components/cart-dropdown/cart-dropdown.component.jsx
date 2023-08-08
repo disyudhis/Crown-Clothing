25b51999217c9b3117be7448ac7f6bfa2503bdb3
Yudhistira Haryatmaka Santoso <84625580+disyudhis@users.noreply.github.com>
2023-08-08 15:30:22 +0700

    adding cart context and shopping icon as well as cart dropdown

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,12 @@
+import "./cart-dropdown.style.scss";
+import Button from "../button/button.component";
+const CartDropdown = () => {
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items"></div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
+
+export default CartDropdown;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,16 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
-
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import "./navigation.style.scss";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  // console.log(currentUser);
-
+  const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
       <div className="navigation">
@@ -29,7 +30,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
